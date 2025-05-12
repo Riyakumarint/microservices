dc96618898e05234cb9781b8bdeae1ece323b42a
riyakumari0524 <rkumari@griddynamics.com>
2025-05-12 22:45:43 +0530

    Enhancing documentation of REST APIs using @OpenAPIDefinition

--- accounts/src/main/java/com/microservices/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/microservices/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.microservices.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservices REST API Documentation",
+				description = "Accounts microservices REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Riya Kumari",
+						email = "riyakumaribkj@gmail.com",
+						url = "https://www.riya.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.riya.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Accounts microservices REST API Documentation",
+				url = "https://www.riya.com/swagger-ui/index.html"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
